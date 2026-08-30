86bf528e33eca40eb42efdd3bdf083b219ec7546
sashi <sashi0034.sashi@gmail.com>
2026-08-30 21:54:42 +0900

    Fix EPWING reader parsing (#744)
    * fix uncompressed EPWING index parsing
    
    * update EPWING documentation
    
    * add English translations, remove backticks around names
    
    * decode EPWING catalog and text data with errors=replace
    
    * raise a clear error for empty EPWING text files
    
    * add pyglossary/plugins/epwing/review.md
    
    ---------
    
    Co-authored-by: Saeed Rasooli <saeed.gnu@gmail.com>

--- pyglossary/plugins/epwing/__init__.py
+++ pyglossary/plugins/epwing/__init__.py
@@ -48,13 +48,19 @@ optionsProp: dict[str, Option] = {}
 
 docTail = """### Supported Dictionaries (Subbooks)
 
-The reader includes specialized extractors for various common EPWING dictionaries:
+The reader includes specialized extractors for the following EPWING subbook titles:
 
-- 広辞苑 (Koujien)
-- 大辞林 (Daijirin)
+- 三省堂　スーパー大辞林 (Sanseido Super Daijirin)
 - 大辞泉 (Daijisen)
-- 明鏡国語辞典 (Meikyou)
-- 学研 (Gakken)
-- 研究社 (Kenkyusha)
-- 故事ことわざの辞典
+- 明鏡国語辞典 (Meikyo Japanese Dictionary)
+- 故事ことわざの辞典 (Dictionary of Idioms and Proverbs)
+- 研究社　新和英大辞典　第５版 (Kenkyusha's New Japanese-English Dictionary,
+  5th Edition)
+- 広辞苑第六版 (Kojien, 6th Edition)
+- 広辞苑　第四版 (Kojien, 4th Edition)
+- 付属資料 (Supplementary Materials)
+- 学研国語大辞典 (Gakken's Great Japanese Dictionary)
+- 古語辞典 (Classical Japanese Dictionary)
+- 故事ことわざ辞典 (Dictionary of Idioms and Proverbs)
+- 学研漢和大字典 (Gakken's Great Kanji-Japanese Dictionary)
 """
